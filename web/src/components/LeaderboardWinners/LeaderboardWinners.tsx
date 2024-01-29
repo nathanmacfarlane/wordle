3a9cf458b42ec2ab4a6c404887051ba558cb9262
= <nathanmmacfarlane@gmail.com>
2024-01-28 21:58:32 -0700

    fix bug

--- web/src/components/LeaderboardWinners/LeaderboardWinners.tsx
+++ web/src/components/LeaderboardWinners/LeaderboardWinners.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react'
+
 import { HStack } from '@chakra-ui/react'
 
 import LeaderboardWinner from '../LeaderboardWinner/LeaderboardWinner'
@@ -11,30 +13,39 @@ export type LeaderboardWinnersProps = {
 }
 
 const LeaderboardWinners: React.FC<LeaderboardWinnersProps> = ({ winners }) => {
-  const firstPlace = winners[0]
-  const secondPlace = winners[1]
-  const thirdPlace = winners[2]
+  const [firstPlace, secondPlace, thirdPlace] = useMemo(() => {
+    const firstPlace = winners.length > 0 ? winners[0] : undefined
+    const secondPlace = winners.length > 1 ? winners[1] : undefined
+    const thirdPlace = winners.length > 2 ? winners[2] : undefined
+    return [firstPlace, secondPlace, thirdPlace]
+  }, [winners])
 
   return (
     <HStack w="full" justifyContent="center" alignItems="flex-end">
-      <LeaderboardWinner
-        place={2}
-        name={secondPlace.name}
-        imageUrl={secondPlace.imageUrl}
-        score={secondPlace.score}
-      />
-      <LeaderboardWinner
-        place={1}
-        name={firstPlace.name}
-        imageUrl={firstPlace.imageUrl}
-        score={firstPlace.score}
-      />
-      <LeaderboardWinner
-        place={3}
-        name={thirdPlace.name}
-        imageUrl={thirdPlace.imageUrl}
-        score={thirdPlace.score}
-      />
+      {secondPlace && (
+        <LeaderboardWinner
+          place={2}
+          name={secondPlace.name}
+          imageUrl={secondPlace.imageUrl}
+          score={secondPlace.score}
+        />
+      )}
+      {firstPlace && (
+        <LeaderboardWinner
+          place={1}
+          name={firstPlace.name}
+          imageUrl={firstPlace.imageUrl}
+          score={firstPlace.score}
+        />
+      )}
+      {thirdPlace && (
+        <LeaderboardWinner
+          place={3}
+          name={thirdPlace.name}
+          imageUrl={thirdPlace.imageUrl}
+          score={thirdPlace.score}
+        />
+      )}
     </HStack>
   )
 }
